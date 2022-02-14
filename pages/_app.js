204216ffaf43d69a7cc1cd8479a2ca9d5070c40a
mason roberts <mrobe18@gmail.com>
2022-02-14 09:14:34 -0500

    added head meta data to main

--- pages/_app.js
+++ pages/_app.js
@@ -14,9 +14,28 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <title>Erudition SAA-C02</title>
+        <meta charset='utf-8' />
+        <link rel='icon' href='/favicon.ico' />
+        <meta
+          name='viewport'
+          content='width=device-width, height=device-height, viewport-fit=cover, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no'
+        />
+        <meta name='mobile-web-app-capable' content='yes' />
+        <meta name='apple-mobile-web-app-capable' content='yes' />
+        <meta
+          property='og:url'
+          content='https://erudition-saa-c02.vercel.app'
+        />
+        <meta property='og:title' content='Erudition SAA-C02' />
+        <meta name='author' content='Mason Roberts' />
         <meta
           name='description'
-          content='Cliff Notes for the Solution Architect Associate Certification course on A Cloud Guru. Brush up on the information required to pass the SAA-C02 Exam. Presented by Developer 3027, Mason Roberts. Give yourself the best shot at passing the AWS certification. Pass the aws saa-c02 certification.'
+          content='Cliff Notes for the Solution Architect Associate Certification course on A Cloud Guru. Brush up on the information required to pass the AWS SAA-C02 Exam. Presented by Developer 3027, Mason Roberts. Give yourself the best shot at passing the AWS certification. Pass the aws saa-c02 certification.'
+        />
+        <meta
+          property='og:image'
+          content='https://dev3027public.s3.amazonaws.com/erudition-card.png'
+          alt='erudition card image'
         />
         <link rel='preconnect' href='https://fonts.google.com' />
         <link
