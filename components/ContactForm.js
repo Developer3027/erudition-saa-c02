c5ea7d325383ac7cd88ffbfc33dd5b3ee710cc9d
mason roberts <mrobe18@gmail.com>
2022-02-16 10:37:09 -0500

    added firebase fitestore to contact form. local works.

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,25 +1,39 @@
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
+import { db } from '../firebase/firebaseConfig';
 import React, { useState } from 'react';
 import styles from '../styles/Contact.module.css';
 
 const ContactForm = () => {
   const [error, setError] = useState('');
   const [success, setSuccess] = useState('');
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState({ name: '', email: '', detail: ''})
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     const emailRE = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-    if (name === '' || email === '' || message === '') {
+    if (message.name === '' || message.email === '' || message.detail === '') {
       setError('Please fill out form');
-    } else if (email.length < 8 && !emailRE.test(email)) {
+    } else if (message.email.length < 8 && !emailRE.test(message.email)) {
       setError('Please enter a valid email');
     } else {
       setError('');
     }
+
+    const collectionRef = collection(db, 'contact')
+    try {
+      const docRef = await addDoc(collectionRef, {
+        ...message, timestamp: serverTimestamp()
+      })
+      setMessage({ name: '', email: '', detail: ''})
+      setSuccess('Thank you, message has been sent.')
+      setTimeout(() => setSuccess(''), 3000)
+    } catch(err) {
+      setError(err);
+    }
+    
+    
   };
 
   return (
@@ -35,8 +49,8 @@ const ContactForm = () => {
             className={styles.contactInput}
             type='text'
             id='name'
-            value={name}
-            onChange={(e) => setName(e.target.value)}
+            value={message.name}
+            onChange={(e) => setMessage({ ...message, name: e.target.value })}
             placeholder='Name'
           />
         </div>
@@ -48,22 +62,22 @@ const ContactForm = () => {
             className={styles.contactInput}
             type='email'
             id='email'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            value={message.email}
+            onChange={(e) => setMessage({ ...message, email: e.target.value })}
             placeholder='Email'
           />
         </div>
         <div className={styles.contactInputWrap}>
-          <label className={styles.contactLabel} htmlFor='message'>
+          <label className={styles.contactLabel} htmlFor='detail'>
             Message
           </label>
           <textarea
             className={styles.contactTextArea}
             cols='4'
             rows='5'
-            id='message'
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
+            id='detail'
+            value={message.detail}
+            onChange={(e) => setMessage({ ...message, detail: e.target.value })}
             placeholder='How can I help you?'
           />
         </div>
